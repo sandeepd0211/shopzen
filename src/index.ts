72742e6be7c093a630a7fdf395b5e197c4b921ef
Sandeep D <sandeepd0211@gmail.com>
2024-03-24 13:23:21 +0530

    chore:Added env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-import express from "express";
+import dotenv from "dotenv";
 import App from "./main";
 
+dotenv.config({
+  path:
+    process.env.NODE_ENV === "production"
+      ? `.env.production`
+      : `.env.development`,
+});
+
 const app = App.getInstance();
-const PORT = new Number(process.env.PORT || 5000).valueOf();
+const PORT = new Number(process.env.PORT).valueOf();
 app.startServer(PORT);
